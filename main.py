4095c5a928fcaea3fad7fa99ba1f9d6784ced5db
Haran Arbel <haranarbel@gmail.com>
2023-12-25 12:23:08 +0200

    containerized app

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 import openai
 from openai import OpenAI
 import os
+from fastapi import FastAPI
 import asyncio
 
+app = FastAPI()
 
-def test():
-    client = OpenAI(
-        api_key=os.environ.get("OPENAI_API_KEY")
-    )
-
-    response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": "Say this is a test",
-            }
-        ],
-        model="gpt-3.5-turbo"
-    )
-
-    return response.choices[0].message.content
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-if __name__ == "__main__":
-    print(test())
+#
+# def test():
+#     client = OpenAI(
+#         api_key=os.environ.get("OPENAI_API_KEY")
+#     )
+#
+#     response = client.chat.completions.create(
+#         messages=[
+#             {
+#                 "role": "user",
+#                 "content": "Say this is a test",
+#             }
+#         ],
+#         model="gpt-3.5-turbo"
+#     )
+#
+#     return response.choices[0].message.content
+
+
+# if __name__ == "__main__":
+    # print(test())
+    # messages = [
+    #     '''[You, 10:05 AM]
+    #         Hey team, I've encountered a few issues that need our attention. The first one is regarding the new deployment script. It seems to be failing at the authentication step. I've double-checked the credentials, but something's still off.
+    #     ''',
+    #     '''
+    #         [Team Member 1, 10:06 AM]
+    #         Good catch! Did you check if the API keys were updated recently?
+    #     ''',
+    #     '''
+    #         [Team Member 2, 10:07 AM]
+    #         I'll take a look at the deployment logs. Maybe there's something we're missing.
+    #     ''',
+    #
+    # ]
